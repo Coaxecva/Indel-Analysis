f69561334273058890a7eeb4c8385824fdb0d5aa
Quang Tran <tmquang@gmail.com>
2016-06-12 00:00:25 -0500

    Update plain2fq.py

--- plain2fq.py
+++ plain2fq.py
@@ -2,6 +2,7 @@ import sys, os
 import mimetypes
 
 pos_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/positions/"
+perl_path = "/home/qmtran/Indel_Analysis/"
 
 def get_filepaths(directory):
     """
@@ -30,7 +31,7 @@ if __name__ == '__main__':
 		if mimetypes.guess_type(f)[0] == 'text/plain':
 			#print(f)
 			init = open(f)
-			fpos = open(f[:-4])
+			fpos = open(pos_path+f[:-4])
 			print ("Opening the file: " + f)
 			target = open(f[:-3]+"fa", 'w')
 			i = 0
@@ -40,11 +41,11 @@ if __name__ == '__main__':
 				pos = fpos.readline()
 				if read == '':
 					break
-				target.write(">" + str(i) + "##" + pos)
+				target.write(">" + str(i) + "##" + pos.strip())
 				target.write("\n")
 				target.write(read.strip())
 				target.write("\n")
 			target.close()
 			init.close()
 			fpos.close()
-			os.system("perl fasta_to_fastq.pl " + f[:-3]+"fa >" + f[:-3]+"fq")
+			os.system("perl " + perl_path + "fasta_to_fastq.pl " + f[:-3]+"fa >" + f[:-3]+"fq")
