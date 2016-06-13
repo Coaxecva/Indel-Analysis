202e894df6392254cf1a97c9ee694e3fda05bcc5
Quang Tran <tmquang@gmail.com>
2016-06-13 16:17:59 -0500

    Update mapped_agreement_INDEL.py

--- mapped_agreement_INDEL.py
+++ mapped_agreement_INDEL.py
@@ -1,8 +1,12 @@
 import sys, os
 
-folder_path = "reads22/alignment/"
+folder_fq_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/reads/"
+folder_sam_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/reads/alignment/"
+flank_match = "21M"
 
-sam_fn = sys.argv[1]
+allowed_dis = 20
+
+fq_list_fn = sys.argv[1]
 
 def get_filepaths(directory):
     """
@@ -26,21 +30,31 @@ def get_filepaths(directory):
 if __name__ == '__main__':
 
 	print ("###########################################################")
-	#Stat:
-	
-	f = open(sys.argv[1])
-	while True:
-		read = f.readline()
-		if read == '':
-			break
 
-		for line in open(sam_fn):
-		    if line[0] == "@":
-		        pass
-		    else:
-		        sl = line.split()
-		        cigar= sl[5]
-		        print(cigar[:3], cigar[:3]=="23M")
+	#Stat:
+	alist = ["Bowtie2.", "SHRiMP.", "Razers3.", "Cushaw2.", "Bwasw.", "Gassst.", "Smalt."]
+	for f in open(fq_list_fn):		
+		total = sum(1 for line in open(folder_fq_path + f.strip()))				
+		for aligner in alist:
+			#print(aligner)
+			agreement = 0
+			mapped = 0
+			for line in open(folder_sam_path + f[:-3] + aligner + "sam"):
+				if line[0] == "@":
+					pass
+				else:
+					sl = line.split()
+					print(sl)
+					#print(int(sl[0][sl[0].find("##")+2:])-int(flank_match[:-1])) #true pos
+					#print(sl[3]) #estimate
+					if (abs(int(sl[0][sl[0].find("##")+2:])-int(flank_match[:-1])-int(sl[3])) < allowed_dis):
+						mapped += 1
+						#print("++")
+					cigar = sl[5]					
+					if (cigar[:3]==flank_match):
+						agreement += 1
+			print(str(total/4)+"\t"+str(mapped)+"\t"+str(agreement))
+			exit()
 
 	print ("###########################################################")
 	
