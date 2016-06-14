b7b88587a2ac733c4ca72edf335740a9bfd6d18c
Quang Tran <tmquang@gmail.com>
2016-06-14 17:52:13 -0500

    Update SR_alignment_runner.py

--- SR_alignment_runner.py
+++ SR_alignment_runner.py
@@ -9,6 +9,7 @@ Cushaw2_path = "~/quang/cushaw2-v2.4.2/cushaw2 align -r "
 Gassst_path = "~/quang/Gassst_v1.28/Gassst -d "
 Razers3_path = "~/quang/razers3 --unique -of sam -o "
 BWASW_path = "~/quang/bwa/bwa bwasw "
+BWA_path = "~/quang/bwa/bwa "
 Smalt_path = "~/quang/smalt map -o "
 
 if __name__ == '__main__':
@@ -19,23 +20,29 @@ if __name__ == '__main__':
 		if read == '':
 			break
 		#print(read.strip())
-		#print(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
-		#os.system(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
+		print(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
+		os.system(SRmapper_path+ref_path+"GRCh37_"+read[:-4]+"-SRmapper { "+read[:-3]+"fastq } "+read[:-4]+".SRmapper.sam &")
 		
 		print(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam")
-		os.system(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam &")
+		os.system(SHRiMp_path+read.strip()+" "+ref_path+"GRCh37_"+read[:-4]+".fasta > "+read[:-4]+".SHRiMP.sam")
 
 		print(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read.strip()+" -S "+read[:-4]+".Bowtie2.sam")
-		os.system(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read.strip()+" -S "+read[:-4]+".Bowtie2.sam")
+		os.system(Bowtie2_path+ref_path+"GRCh37_"+read[:-4]+"-bowtie2 "+read.strip()+" -S "+read[:-4]+".Bowtie2.sam &")
 		
 		print(Cushaw2_path+ref_path+"GRCh37_"+read[:-4]+".fasta -f "+read.strip()+" -o "+read[:-4]+".Cushaw2.sam")
 		os.system(Cushaw2_path+ref_path+"GRCh37_"+read[:-4]+".fasta -f "+read.strip()+" -o "+read[:-4]+".Cushaw2.sam &")
 		
 		print(Gassst_path+ref_path+"GRCh37_"+read[:-4]+".fasta -i "+read[:-4]+".fa -o "+read[:-4]+".Gassst -p 90 -h 1 && ~/quang/Gassst_v1.28/gassst_to_sam "+read[:-4]+".Gassst "+read[:-4]+".Gassst.sam")
-		os.system(Gassst_path+ref_path+"GRCh37_"+read[:-4]+".fasta -i "+read[:-4]+"fa -o "+read[:-4]+".Gassst -p 90 -h 1 && ~/quang/Gassst_v1.28/gassst_to_sam "+read[:-4]+".Gassst "+read[:-4]+".Gassst.sam &")
+		os.system(Gassst_path+ref_path+"GRCh37_"+read[:-4]+".fasta -i "+read[:-4]+".fa -o "+read[:-4]+".Gassst -p 90 -h 1 && ~/quang/Gassst_v1.28/gassst_to_sam "+read[:-4]+".Gassst "+read[:-4]+".Gassst.sam")
 		
 		print(Razers3_path+read[:-4]+".Razers3.sam "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip())
 		os.system(Razers3_path+read[:-4]+".Razers3.sam "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" &")
+
+		print(BWA_path+" mem "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwamem.sam")
+		os.system(BWA_path+" mem "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwamem.sam &")
+
+		print(BWA_path+" aln "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwa.sai && "+BWA_path+" samse "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read[:-4]+".Bwa.sai "+read.strip()+" > "+read[:-4]+".Bwa.sam ")
+		os.system(BWA_path+" mem "+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwa.sam &")
 
 		print(BWASW_path+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwasw.sam")
 		os.system(BWASW_path+ref_path+"GRCh37_"+read[:-4]+".fasta "+read.strip()+" > "+read[:-4]+".Bwasw.sam &")
