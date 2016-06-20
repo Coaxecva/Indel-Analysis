dafc23f2dd47e288f5ccf12bbe7038240a0bac63
Quang Tran <tmquang@gmail.com>
2016-06-20 12:28:30 -0500

    Update Prob_agreement_by_chance.py

--- Prob_agreement_by_chance.py
+++ Prob_agreement_by_chance.py
@@ -3,7 +3,8 @@ import sys, os
 flank_match = "51M"
 allowed_dis = 20
 
-outs_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/reads50/"
+outs_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/outs50/"
+fq_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/reads50/"
 sam_path = "/home/qmtran/Indel_Analysis/Data_reads22_29/reads50/alignment/"
 
 fq_list_fn = sys.argv[1]
@@ -25,30 +26,26 @@ if __name__ == '__main__':
 	#alist = ["Smalt."]
 	alist = ["Bowtie2.", "SHRiMP.", "Razers3.", "Cushaw2.", "Bwasw.", "Bwamem.", "Bwa.", "Gassst.", "Smalt."]
 
-	f = open(fq_list_fn)
-	while True:
-		fname = f.readline().strip()
-		if fname == '':
-			break
-
 	for f in open(fq_list_fn):		
-	total = sum(1 for line in open(folder_fq_path + f.strip()))				
-	idx = -1
-	for aligner in alist:
-		idx += 1
-		#print(aligner)
-		agreement = 0
-		mapped = 0
-		for line in open(folder_sam_path + f[:-3] + aligner + "sam"):
-			if line[0] == "@":
-				pass
-			else:
-				sl = line.split()
-				#print(f)
-				#print(sl)
-				#print(int(sl[0][sl[0].find("##")+2:])-int(flank_match[:-1])) #true pos
-				#print(sl[3]) #estimate
-				if (sl[3] == ''):
-					continue
+		total = sum(1 for line in open(fq_path + f.strip()))				
+		idx = -1
+		for aligner in alist:
+			idx += 1
+			#print(aligner)
+			agreement = 0
+			mapped = 0
+			for line in open(sam_path + f[:-3] + aligner + "sam"):
+				if line[0] == "@":
+					pass
+				else:
+					sl = line.split()
+					#print(f)
+					#print(sl)
+					#print(int(sl[0][sl[0].find("##")+2:])-int(flank_match[:-1])) #true pos
+					#print(sl[3]) #estimate
+					if (sl[3] == ''):
+						continue
+
+			print(str(total/4))
 
 	print ("###########################################################")
